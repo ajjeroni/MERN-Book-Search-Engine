1978874c4eceeb7c2b229545882e8229fa4c420c
Ajjeroni <adanjeronimo213@gmail.com>
2023-08-24 15:28:45 -0700

    finished resolvers from server side

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -15,19 +15,14 @@ const resolvers = {
   Mutation: {
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
-
       if (!user) {
         throw new AuthenticationError("No user found with this email address");
       }
-
       const correctPw = await user.isCorrectPassword(password);
-
       if (!correctPw) {
         throw new AuthenticationError("Incorrect credentials");
       }
-
       const token = signToken(user);
-
       return { token, user };
     },
     addUser: async (parent, args) => {
@@ -60,23 +55,17 @@ const resolvers = {
       }
     },
     removeBook: async (parent, { bookId }, context) => {
-      if (!context.user) {
-        throw new Error("Authentication required");
-      }
-      try {
-        
-        const updatedUser = await User.findByIdAndUpdate(
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $pull: { savedBooks: bookId } },
-          { new: true, runValidators: true }
-        )
-        if (!updatedUser) {
-          throw new Error("User not found");
-        }
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+
         return updatedUser;
-      } catch (error) {
-        throw new Error(error.message);
       }
+
+      throw AuthenticationError;
     },
   },
 };
