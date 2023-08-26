1bb20f3b490d74078902282883fcb4179048d2a5
Adan J <adanjeronimo213@gmail.com>
2023-08-25 21:22:51 -0700

    worked on client side component/loginForm

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -2,13 +2,16 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
+import {useMutation}  from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, { error }] = useMutation(LOGIN_USER)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,7 +29,9 @@ const LoginForm = () => {
     }
 
     try {
-      // const response = await loginUser(userFormData);
+      const response = await login({
+        variables: {...userFormData}
+      })
 
       // if (!response.ok) {
       //   throw new Error('something went wrong!');
@@ -35,6 +40,8 @@ const LoginForm = () => {
       // const { token, user } = await response.json();
       // console.log(user);
       // Auth.login(token);
+      // console.log(response.data.loginUser.token);
+      Auth.login(response.data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
